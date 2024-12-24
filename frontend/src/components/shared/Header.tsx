b970a722342a416e77a62671886c4dc9998578dc
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-12-24 12:04:20 +0000

    Create header ui

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
           <h1 className='md:font-play italic text-3xl'>sticky</h1>
         </div>
 
-        <div className='md:flex items-center md:relative font-nunito text-2xl'>
+        <div className='md:flex items-center md:relative font-nunito text-[18px] uppercase'>
         <div className='h-16 border-b border-r ' />
            <div className='px-5'><h1>about</h1></div>  
            <div className='h-16 border-b border-r' />
