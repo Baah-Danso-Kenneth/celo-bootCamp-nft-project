669f4f5cf6e5dfe1e8233f4e64f981e4154e4385
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-02 14:38:52 +0000

    Create sidebar and maincontent

---
+++ frontend/src/app/createNFT/page.tsx
@@ -0,0 +1,13 @@
+import CreateNFT from '@/components/sections/createNFT'
+import MainContent from '@/components/sections/createNFT/MainContent'
+import React from 'react'
+
+function page() {
+  return (
+      <>
+      <MainContent/>
+      </>
+  )
+}
+
+export default page
