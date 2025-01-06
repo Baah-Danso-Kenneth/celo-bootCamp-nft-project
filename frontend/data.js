a51439875de56a29b3184063b1493c3e73162022
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-06 12:53:52 +0000

    Hard coded data for building dapp

---
+++ frontend/data.js
@@ -0,0 +1,87 @@
+import { ArrowDownRight } from "lucide-react"   
+  
+
+ export const sociaLinks=[
+   {name:'discord', icon: <ArrowDownRight className='h-6'/>,href:'#'},
+   {name:'twitter', icon:<ArrowDownRight className='h-6'/>,href:'#'},
+   {name:'linkedIn', icon:<ArrowDownRight className='h-6'/>, href:'#'},
+   {name:'Instagram', icon:<ArrowDownRight className='h-6'/>, href:'#'}
+
+]
+
+
+export const navLinks=[
+  {name:'home', href:"#"},
+  {name:'market', href:"/marketplace"},
+  {name:'collections', href:"#"},
+  {name:'createNft', href:"#"},
+  {name:'about', href:"#"},
+  {name:'about', href:"#"},
+  {name:'about', href:"#"},
+  {name:'about', href:"#"},
+]
+
+
+export const carouselNft = [
+  {
+    name: 'gaming',
+    title: 'Altro-verse',
+    image: '/images/cr-1.jpeg',
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'fiction',
+    title: 'Weeney Azure',
+    image: '/images/cr-2.jpeg',
+    description: 'altro modern games specs to reach the height altro modern games specs to reach the height altro modern games specs to reach the height',
+  },
+  {
+    name: 'horror',
+    title: 'Glucian',
+    image: '/images/cr-3.jpeg', 
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'action',
+    title: 'Alcianoro rezzi',
+    image: '/images/cr-4.jpg',
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'action',
+    title: 'Questino Fixa',
+    image: '/images/cr-5.jpg',
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'fiction',
+    title: 'Untitled Fiction', 
+    image: '/images/cr-6.jpeg',
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'game',
+    title: 'Octapus Goaty',
+    image: '/images/cr-7.jpeg',
+    description: 'altro modern games specs to reach the height',
+  },
+
+  {
+    name: 'action',
+    title: 'Rebotro Ocian',
+    image: '/images/cr-8.jpg',
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'game',
+    title: 'Rider of Pensunia',
+    image: '/images/cr-9.jpg',
+    description: 'altro modern games specs to reach the height',
+  },
+  {
+    name: 'fiction',
+    title: 'Mariline Orazzy',
+    image: '/images/cr-10.jpeg',
+    description: 'altro modern games specs to reach the height',
+  },
+];
