25583b6fcc84c58130a3841c8313fe958fcaab14
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-06 22:38:28 +0000

    Update types

--- frontend/types/types.ts
+++ frontend/types/types.ts
@@ -1,8 +1,16 @@
 
 
-export type NftCarousel = {
+export type NftCarouselProp = {
   name: string;
   title:string;
   image: string;
   description:string;
+  price?:string;
+  date_created?:string;
+  category?:string;
+  buzz?:string;
+  sold?:boolean;
+  isVertical?:boolean;
+  promo?:boolean
 }
+
